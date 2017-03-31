cc3836157aadfcce6dcece30ad5f350d35e3e61c
Sofi Rivas <sofi.rivasm@gmail.com>
2017-03-31 16:47:33 -0600

    Add video html tag to use in search result component

--- src/components/search-result.vue
+++ src/components/search-result.vue
@@ -1,22 +1,31 @@
 <!-- Component Template -->
 <template>
-    <div class="cat-search-container">
-      <p>Resultado</p>
-      <img v-bind:src="imageSource">
-    </div>
+  <div>
+      <video width="320" height="240" autoplay v-for="gif in gifsData" class="gif-tile">
+      <source :src="gif.images.downsized_small.mp4" type="video/mp4">
+        Tu navegador no soporta la etiqueta de video HTML, no gifs for you my friend.
+    </video>
+  </div>
 </template>
 
 <!-- Component Script -->
 <script>
   export default {
-    name: 'search-result',
+    name: 'search-tile',
     props: {
-      imageSource: ''
+      gifsData: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      }
     }
   }
 </script>
 
 <!-- If necessary, component styles -->
 <style scoped>
-
+  .gif-tile {
+    margin: 10px;
+  }
 </style>
